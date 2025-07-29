ed61d43a2dab05085606de0e8355f4646b4d6a0b
Rob Caelers <rob.caelers@gmail.com>
2025-07-29 12:21:44 +0200

    fix copyright

--- src/BundleHelper.cc
+++ src/BundleHelper.cc
@@ -1,4 +1,4 @@
-// Copyright (C) 202Caelers <rob.caelers@gmail.com>
+// Copyright (C) 2025 Rob Caelers <rob.caelers@gmail.com>
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
